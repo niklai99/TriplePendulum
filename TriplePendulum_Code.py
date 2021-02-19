67d6a4fb2683c1ad324dd249910d033bd254af68
Nik <62773963+niklai99@users.noreply.github.com>
2021-02-20 00:22:36 +0100

    Updated animations

--- TriplePendulum_Code.py
+++ TriplePendulum_Code.py
@@ -227,3 +227,61 @@ def animate_pendulum(n, times, initial_positions, initial_velocities, lengths, m
                                    blit=True, init_func=init)
     plt.close(fig)
     return anim
+
+
+
+
+def animate_pendulums(n, times, initial_positions, initial_velocities, lengths, masses, n_pendulums=1, perturbation=0, track_length=15):
+    oversample = 3
+    track_length *= oversample
+    
+    t = np.linspace(0, 10, oversample * 200)
+    p = [integrate_pendulum(n=n, times=times, 
+                            initial_positions=initial_positions + i * perturbation / n_pendulums, 
+                            initial_velocities=initial_velocities, lengths=lengths, masses=masses)
+         for i in range(n_pendulums)]
+
+    positions = np.array([get_xy_coords(pi) for pi in p])
+    positions = positions.transpose(0, 2, 3, 1)
+    # positions is a 4D array: (npendulums, len(t), n+1, xy)
+    
+    fig = plt.figure( figsize=(6, 6) )
+    ax = fig.add_subplot(1, 1, 1)
+
+    ax.axes.xaxis.set_ticks([])
+    ax.axes.yaxis.set_ticks([])
+
+    ax.set(xlim=(-1, 1), ylim=(-1, 1))
+    
+    track_segments = np.zeros((n_pendulums, 0, 2))
+    tracks = collections.LineCollection(track_segments, cmap='gist_rainbow')
+    tracks.set_array(np.linspace(0, 1, n_pendulums))
+    ax.add_collection(tracks)
+    
+    points, = plt.plot([], [], 'ok')
+    
+    pendulum_segments = np.zeros((n_pendulums, 0, 2))
+    pendulums = collections.LineCollection(pendulum_segments, colors='black')
+    ax.add_collection(pendulums)
+
+    def init():
+        pendulums.set_segments(np.zeros((n_pendulums, 0, 2)))
+        tracks.set_segments(np.zeros((n_pendulums, 0, 2)))
+        points.set_data([], [])
+        return pendulums, tracks, points
+
+    def animate(i):
+        i = i * oversample
+        pendulums.set_segments(positions[:, i])
+        sl = slice(max(0, i - track_length), i)
+        tracks.set_segments(positions[:, sl, -1])
+        x, y = positions[:, i].reshape(-1, 2).T
+        points.set_data(x, y)
+        return pendulums, tracks, points
+
+    interval = 1000 * oversample * t.max() / len(t)
+    anim = animation.FuncAnimation(fig, animate, frames=len(times) // oversample,
+                                   interval=interval,
+                                   blit=True, init_func=init)
+    plt.close(fig)
+    return anim
